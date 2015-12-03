29444113f505c80acbfc4614e2c830bffd07e5a1
kukun99 <naka99@gmail.com>
2015-12-03 02:35:01 +0000

    Localization jp

--- app/admin/reservation.rb
+++ app/admin/reservation.rb
@@ -13,5 +13,32 @@ ActiveAdmin.register Reservation do
 #   permitted
 # end
 
+permit_params :customer_id, :event_id, :buy_count, :payment
+
+  index title: I18n.t('activerecord.models.reservation') + '一覧' do
+    selectable_column
+    id_column
+    column :customer_id
+    column :event_id
+    column :buy_count
+    column :payment
+    actions
+  end
+
+  payment_list = { "現金": "現金", "振込": "振込", "クレジットカード": "クレジットカード" }.invert
+  filter :customer_id, as: :select, collection: Customer.all.map { |c| [c.name, c.id] }
+  filter :event_id, as: :select, collection: Event.all.map { |e| [e.title, e.id] }
+  filter :buy_count
+  filter :payment, as: :select, collection: payment_list
+
+  form do |f|
+    f.inputs "予約登録" do
+      f.input :customer_id, as: :select, collection: Customer.all.map { |c| [c.name, c.id] }, include_blank: false
+      f.input :event_id, as: :select, collection: Event.all.map { |e| [e.title, e.id] }, include_blank: false
+      f.input :buy_count
+      f.input :payment, as: :select, collection: payment_list, include_blank: false
+    end
+    f.actions
+  end
 
 end
