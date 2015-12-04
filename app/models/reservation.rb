a418deeca6fae11cb015a35d17ee90280ca47806
Inoue-h <Inoue@test.com>
2015-12-04 04:44:09 +0000

    seed修正ほか

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,11 @@
 class Reservation < ActiveRecord::Base
+  PAYMENT_TYPES = [ "現金", "銀行振込", "クレジットカード" ]
+
   belongs_to :customer
   belongs_to :event
+
+  validates :customer_id, presence: true
+  validates :event_id, presence: true
+  validates :buy_count, presence: true
+  validates :payment, inclusion: PAYMENT_TYPES
 end
