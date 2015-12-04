e0787a9c6cbec4dff09c8cbaebec38ecb8a1a73d
Inoue-h <Inoue@test.com>
2015-12-04 05:51:36 +0000

    annotate,link

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  date        :datetime
+#  stock_count :integer
+#  price       :integer
+#  sell_count  :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
--- test/models/reservation_test.rb
+++ test/models/reservation_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: reservations
+#
+#  id          :integer          not null, primary key
+#  customer_id :integer
+#  event_id    :integer
+#  buy_count   :integer
+#  payment     :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class ReservationTest < ActiveSupport::TestCase
